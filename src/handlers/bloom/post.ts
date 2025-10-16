7933d0175bc1f3a2f30f75eb4aa0a1df2a118aa4
eirikhanasand <eirik.hanasand@gmail.com>
2025-10-16 02:07:29 +0200

    Improved checks

--- src/handlers/bloom/post.ts
+++ src/handlers/bloom/post.ts
@@ -5,18 +5,30 @@ import pkg from 'bloom-filters'
 const { BloomFilter } = pkg
 
 export default async function bloomHandler(req: FastifyRequest, res: FastifyReply) {
+    const { password } = (await req.body as { password: string }) || {}
+    if (!password) {
+        return res.send({ ok: false, reason: 'No password provided.' })
+    }
+
     const BLOOM_DIR = 'bloom'
     const files = fs.readdirSync(BLOOM_DIR).filter(f => f.endsWith('.bloom'))
 
-    const blooms: InstanceType<typeof BloomFilter>[] = files.map(file => {
+    const blooms = files.map(file => {
         const data = fs.readFileSync(path.join(BLOOM_DIR, file))
-        return BloomFilter.fromJSON(JSON.parse(data.toString()))
+        const filter = BloomFilter.fromJSON(JSON.parse(data.toString()))
+        return { file, filter }
     })
 
-    const { password } = (await req.body as { password: string }) || {}
-    const isLeaked = blooms.some(bloom => bloom.has(password))
-    if (isLeaked) {
-        return res.send({ ok: false, reason: 'Known compromised password' })
+    const matches = blooms
+        .filter(({ filter }) => filter.has(password))
+        .map(({ file }) => file)
+
+    if (matches.length > 0) {
+        return res.send({
+            ok: false,
+            reason: 'Known compromised password',
+            sourceFiles: matches
+        })
     }
 
     return res.send({ ok: true })
