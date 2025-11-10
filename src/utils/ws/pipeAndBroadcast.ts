9864dbc5e41814ac77980bf8988f4dacf20b61ca
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-10 06:21:19 +0100

    Added find_all script

--- src/utils/ws/pipeAndBroadcast.ts
+++ src/utils/ws/pipeAndBroadcast.ts
@@ -2,15 +2,7 @@ import { spawn } from 'child_process'
 import broadcast, { clientsMap } from './broadcast.ts'
 
 export default function execPipeAndBroadcast(id: string, password: string): void {
-    const child = spawn('rg',
-        ['-a', '-x', '-n', '--max-depth', '1', '--', password],
-        {
-            stdio: ['ignore', 'pipe', 'pipe'],
-            cwd: `${process.cwd()}/passwords`
-        }
-    )
-
-    child.stdout.on('data', (data: Buffer) => {
+    function parseLine(data: Buffer<ArrayBufferLike>) {
         const text = data.toString()
         const lines = text.split('\n').filter(Boolean)
         lines.forEach(line => {
@@ -24,13 +16,37 @@ export default function execPipeAndBroadcast(id: string, password: string): void
                 })
             }
         })
+    }
+
+    const child = spawn('rg',
+        ['-a', '-x', '-n', '--max-depth', '1', '--', password],
+        {
+            stdio: ['ignore', 'pipe', 'pipe'],
+            cwd: `${process.cwd()}/passwords`
+        }
+    )
+
+    const findChild = spawn('bash', ['./find_all.sh', password], {
+        stdio: ['ignore', 'pipe', 'pipe'],
+        cwd: `${process.cwd()}/passwords`
     })
 
+    child.stdout.on('data', (data: Buffer) => parseLine(data))
     child.stderr.on('data', (err: Buffer) => {
         broadcast(id, 'update', { error: err.toString() })
     })
 
     child.on('close', () => {
+        broadcast(id, 'update', { done: true }, true)
+        clientsMap.delete(id)
+    })
+
+    findChild.stdout.on('data', (data: Buffer) => parseLine(data))
+    findChild.stderr.on('data', (err: Buffer) => {
+        broadcast(id, 'update', { error: err.toString() })
+    })
+
+    findChild.on('close', () => {
         broadcast(id, 'update', { done: true }, true)
         clientsMap.delete(id)
     })
